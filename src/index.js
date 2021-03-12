1c38035421a76e27514c9f1691a5e849888ec8b2
Russell <russelldev@protonmail.com>
2021-03-12 15:35:55 +0000

    Add ApprovalCard.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const comment = [
     "      This HTML file is a template. If you open it directly in the browser, you will see an empty page.",
@@ -13,26 +14,33 @@ const App = () => {
 
     return ( 
          <div className="ui container comments">
-             <CommentDetail 
-                author="Sam"
-                timeAgo="Today at 15:32" 
-                comment={comment[0]} 
-                img={faker.image.image()}
-                />
+             <ApprovalCard>
+                <CommentDetail 
+                    author = "Sam"
+                    timeAgo = "Today at 15:32" 
+                    comment = { comment[0] } 
+                    img = { faker.image.image() }
+                    />
+            </ApprovalCard>
 
-             <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 17:35" 
-                comment={comment[1]}
-                img={faker.image.image()}
-                />
-             
-             <CommentDetail 
-                author="Jane" 
-                timeAgo="Yesterday at 11:22" 
-                comment={comment[2]}
-                img={faker.image.image()}
-                />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 17:35" 
+                    comment={comment[1]}
+                    img={faker.image.image()}
+                    />                
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 11:22" 
+                    comment={comment[2]}
+                    img={faker.image.image()}
+                    />
+            </ApprovalCard>
+                
          </div>         
     );
 };
